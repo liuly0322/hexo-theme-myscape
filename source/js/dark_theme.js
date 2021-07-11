b29e005a234ef7f2570ace426c2be0efaf76d71a
liuly0322 <liuly0322@mail.ustc.edu.cn>
2021-07-11 18:10:33 +0800

    datk theme will refer to system

--- source/js/dark_theme.js
+++ source/js/dark_theme.js
@@ -1,16 +1,21 @@
 function changeDarkTheme() {
-    document.querySelector('html').classList.toggle('dark-theme');
-    if(document.querySelector('html').classList.contains('dark-theme')){
-        localStorage.setItem("hexoTheme", "1");
+    document.documentElement.classList.toggle('dark-theme');
+    if(document.documentElement.classList.contains('dark-theme')){
+        sessionStorage.setItem("hexoTheme", "1");
     } else {
-        localStorage.setItem("hexoTheme", "0");
+        sessionStorage.setItem("hexoTheme", "0");
     }
 };
 
-if(localStorage.getItem("hexoTheme") === null){   //第一次访问网站，默认浅色模式
-    localStorage.setItem("hexoTheme", "0");
+if(sessionStorage.getItem("hexoTheme") === null){   //第一次访问网站，试图参考系统设定
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefersDarkScheme.matches) {
+        sessionStorage.setItem("hexoTheme", "1");
+    } else {
+        sessionStorage.setItem("hexoTheme", "0");
+    }
 } else {
-    const theme = localStorage.getItem("hexoTheme");    //每次打开网站，判断是否需要更新成深色模式
+    const theme = sessionStorage.getItem("hexoTheme");    //每次打开网站，判断是否需要更新成深色模式
     if(theme == 1){
         changeDarkTheme();
     }
