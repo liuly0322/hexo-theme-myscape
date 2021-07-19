75d22c5420349dd1de861e45c39fcf6cbb6884d4
liuly0322 <liuly0322@mail.ustc.edu.cn>
2021-07-19 22:27:28 +0800

    new codeblocks

--- source/js/script.js
+++ source/js/script.js
@@ -129,3 +129,78 @@ if (sessionStorage.getItem("word") === null || sessionStorage.getItem("word") ==
   document.getElementById('hitokoto').innerText = sessionStorage.getItem("word");
   document.getElementById('from-work').innerText = sessionStorage.getItem("work");
 }
+
+$(function () {
+  $('.highlight').wrap('<div class="code-area" style="position: relative"></div>');
+});
+
+// 代码块一键复制
+$(function () {
+  var $copyIcon = $('<span class="fas fa-copy code_copy" title="复制代码" aria-hidden="true"></span>')
+  var $notice = $('<div class="codecopy_notice"></div>')
+  $('.code-area').prepend($copyIcon)
+  $('.code-area').prepend($notice)
+  // “复制成功”字出现
+  function copy(text, ctx) {
+      if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+          try {
+              document.execCommand('copy') // Security exception may be thrown by some browsers.
+              $(ctx).prev('.codecopy_notice')
+                  .text("复制成功")
+                  .animate({
+                      opacity: 1,
+                      top: 30
+                  }, 450, function () {
+                      setTimeout(function () {
+                          $(ctx).prev('.codecopy_notice').animate({
+                              opacity: 0,
+                              top: 0
+                          }, 650)
+                      }, 400)
+                  })
+          } catch (ex) {
+              $(ctx).prev('.codecopy_notice')
+                  .text("复制失败")
+                  .animate({
+                      opacity: 1,
+                      top: 30
+                  }, 650, function () {
+                      setTimeout(function () {
+                          $(ctx).prev('.codecopy_notice').animate({
+                              opacity: 0,
+                              top: 0
+                          }, 650)
+                      }, 400)
+                  })
+              return false
+          }
+      } else {
+          $(ctx).prev('.codecopy_notice').text("浏览器不支持复制")
+      }
+  }
+  // 复制
+  $('.code-area .fa-copy').on('click', function () {
+      var selection = window.getSelection()
+      var range = document.createRange()
+      range.selectNodeContents($(this).siblings('.highlight').find('pre')[0])
+      selection.removeAllRanges()
+      selection.addRange(range)
+      var text = selection.toString()
+      copy(text, this)
+      selection.removeAllRanges()
+  })
+});
+
+// 代码块语言识别
+$(function () {
+  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
+  $('.highlight').before($highlight_lang);
+  $('figure.highlight').each(function () {
+    var code_language = $(this).attr('class');
+    if (!code_language) {
+      return true;
+    };
+    var lang_name = code_language.slice(10);
+    $(this).parent().find(".code_lang").text(lang_name);
+  });
+});
